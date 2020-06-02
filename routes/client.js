4f1eb9c5a5e5f4de49cf0fd3386e7d6e5d6f2ee0
chige007 <461841500@qq.com>
2020-06-03 00:53:26 +0800

    update: 完成新闻中心、合作模式、发展历程

--- routes/client.js
+++ routes/client.js
@@ -20,6 +20,33 @@ router.get('/:lang', function(req, res, next) {
   });
 });
 
+// 核心价值
+router.get('/core_value/:lang', function(req, res, next) {
+    res.render('client/core_value', {
+      menus,
+      lang: req.params.lang,
+      currentMenu: 0
+  });
+});
+
+// 发展历程
+router.get('/development_history/:lang', function(req, res, next) {
+    res.render('client/development_history', {
+        menus,
+        lang: req.params.lang,
+        currentMenu: 0
+    });
+});
+
+// 创新
+router.get('/innovate/:lang', function(req, res, next) {
+    res.render('client/innovate', {
+        menus,
+        lang: req.params.lang,
+        currentMenu: 3
+    });
+});
+
 // 集团业务
 router.get('/business/:lang', function(req, res, next) {
   res.render('client/business', {
@@ -29,12 +56,21 @@ router.get('/business/:lang', function(req, res, next) {
   });
 });
 
-// 核心价值
-router.get('/core_value/:lang', function(req, res, next) {
-  res.render('client/core_value', {
+// 合作模式
+router.get('/cooperation_mode/:lang', function(req, res, next) {
+  res.render('client/cooperation_mode', {
       menus,
       lang: req.params.lang,
-      currentMenu: 0
+      currentMenu: 1
+  });
+});
+
+// 新闻中心
+router.get('/information_center/:lang', function(req, res, next) {
+    res.render('client/information_center', {
+        menus,
+      lang: req.params.lang,
+      currentMenu: 2
   });
 });
 
