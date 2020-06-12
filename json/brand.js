bb2e57133388d802d910609f3764ec2d9752ba8b
chige007 <461841500@qq.com>
2020-06-12 13:26:07 +0800

    品牌修改

--- json/brand.js
+++ json/brand.js
@@ -3,7 +3,23 @@ const text = {
         cn: '品牌',
         en: 'Brands',
         hk: '品牌'
-    }
+    },
+    brands: [{
+        bg: '/images/bg_brand_1.jpg',
+        link: '//www.winmaxsport.com'
+    }, {
+        bg: '/images/bg_brand_2.jpg',
+        link: '//www.mysisia.com'
+    }, {
+        bg: '/images/bg_brand_3.jpg',
+        link: '//www.neo-splash.com'
+    }, {
+        bg: '/images/bg_brand_4.jpg',
+        link: '//shop40355980.m.youzan.com/v2/showcase/feature?alias=Q0dSLFiiZl&sf=wx_menu'
+    }, {
+        bg: '/images/bg_brand_5.jpg',
+        link: ''
+    }]
 }
 
 module.exports = text;
